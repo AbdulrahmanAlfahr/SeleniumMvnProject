d8af55c4810063a9187df54882b0c1d86352a434
abdulrahman <a.alqahtani392@hotmail.com>
2025-09-03 16:35:14 +0300

    1

---
+++ Task06.java
@@ -0,0 +1,26 @@
+package Tasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task06 {
+    public static void main(String[] args) throws InterruptedException {
+
+        // T06//:
+        WebDriver driver = new ChromeDriver();
+
+        // Go to the Amazon URL: https://www.amazon.com//
+        driver.get("https://www.amazon.com/");
+
+
+        //Verify that it writes "Performance Metrics" or " Gateway" from the Source code
+        String pageSource = driver.getPageSource();
+
+
+
+
+
+        Thread.sleep(1000);
+
+    }
+}
